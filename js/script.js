ef862d85143e9d1a152df5da76b4c2dc9ee38a30
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-20 14:53:28 +0100

    riscrizione e commentazione del codice js

--- js/script.js
+++ js/script.js
@@ -1,72 +1,78 @@
+// Descrizione:**
+/*
+Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km(0.21 € al km)
+va applicato uno sconto del 20 % per i minorenni
+va applicato uno sconto del 40 % per gli over 65.
+
+    MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone(non stilizzati), realizziamo le specifiche scritte sopra.
+    La risposta finale(o output) sarà anch’essa da scrivere in console.
+
+    MILESTONE 2: Solo una volta che il milestone 1 sarà completo e funzionante allora finalizzeremo il layout di pagina, in cui l’utente vedrà il form stilizzato e tutto l’output stampato in pagina(il prezzo dovrà sempre essere formattato con massimo due decimali).
+*/
 // variable definitions
 const priceForKm = 0.21;
 let discount = 0;
 
-//buttons variable definition 
+// Declarations of buttons variables
 
-let ticketGenerator = document.getElementById('confirm');
-let ticketReset = document.getElementById('resetAll');
+const btnConfirm = document.getElementById('confirm');
+const resetAll = document.getElementById('resetAll');
 
-// button confirm event listener
+// bntConfirm add event listener function
 
-ticketGenerator.addEventListener('click',
+btnConfirm.addEventListener('click',
 
-    function() {
-        //variable definitions
+    function () {
+        //user variables declaration
         let userName = document.getElementById('nameSurname').value;
-        let userDistance = parseInt(document.getElementById('distance').value);
+        let userDistance = document.getElementById('distance').value;
         let userAge = document.getElementById('userAge').value;
 
+        //  debug (control user output)
         console.log(userName);
         console.log(userDistance);
         console.log(userAge);
 
-        //discount function 
-
+        //discount application
         if (userAge == '1') {
             discount = 0.2;
-        } else if (userAge == '2'){
+        } else if (userAge == '2') {
             discount = 0.4;
         } else {
-            discount= 0;
+            discount = 0;
         }
 
-        //ticket price 
+        // ticket price 
 
         let ticketPrice = (userDistance * priceForKm);
         ticketPrice -= ticketPrice * discount;
 
-        // controllo output
-
-        console.log('Nome user ' + userName);
-        console.log('distanza da percorrere: ' + userDistance);
-        console.log('eta viaggiatore: ' + userAge);
-        console.log('Sconto applicato : ' + discount);
-        console.log(`Totale= ${ticketPrice}`);
-
-        // output su biglietto
+        if (userAge == '1') {
+            document.getElementById('kindOfOffer').innerHTML = 'Sconto Under18 (-20%)';
+        } else if (userAge == '2') {
+            document.getElementById('kindOfOffer').innerHTML = 'Sconto Senior (-40%)';
+        } else {
+            document.getElementById('kindOfOffer').innerHTML = 'Tariffa Standard';
+        }
+        //User info print output on hidden ticket
 
         document.getElementById('passengerName').innerHTML = userName;
-        document.getElementById('ticket_prc').innerHTML = (ticketPrice).toFixed(2) + "€";
         document.getElementById('carriageNum').innerHTML = Math.floor((Math.random() * 10) + 1);
         document.getElementById('trainCode').innerHTML = Math.floor((Math.random() * 1000) + 1);
+        document.getElementById('ticket_prc').innerHTML = (ticketPrice).toFixed(2);
 
-            // applicated discount 
-            if (userAge == '1') {
-                document.getElementById('kindOfOffer').innerHTML = 'Sconto Under18 (-20%)';
-            } else if (userAge == '2'){
-                document.getElementById('kindOfOffer').innerHTML = 'Sconto Senior (-40%)';
-            } else {
-                document.getElementById('kindOfOffer').innerHTML = 'Tariffa Standard';
-            }
-        
-
-
-        //ticket preview 
-
-
-        const ticketShow = document.getElementById('container_hidden');
-        ticketShow.style.display = 'block'
+        let hiddenTicket = document.getElementById('container_hidden');
+        hiddenTicket.style.display = 'block';
     }
 
-)
+);
+// Reset all add event listener funcion
+
+resetAll.addEventListener('click',
+    function () {
+        history.go(0);
+    }
+);
